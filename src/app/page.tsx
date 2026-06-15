dc87020eb08444dec0a3d5671fe84ffa34facf17
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2026-06-14 22:38:22 -0300

    feat: add JsonLd component for structured data in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import Hero from "./views/hero";
 import Discover from "./views/discover";
 import WhyShouldHaveCat from "./views/why-should-have-cat";
-import { getBreedById } from "./libs/the-cat-api";
+import { siteConfig, siteUrl } from "./libs/site";
+import JsonLd from "./components/json-ld";
 
 export default async function Home() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: siteConfig.name,
+    alternateName: siteConfig.title,
+    description: siteConfig.description,
+    url: siteUrl,
+  };
+
   return (
     <main className="mx-4 sm:mx-8 md:mx-12">
+      <JsonLd data={jsonLd} />
       <Hero />
       <Discover />
       <WhyShouldHaveCat />
